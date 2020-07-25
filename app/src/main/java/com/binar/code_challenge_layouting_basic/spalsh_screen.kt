f29f86a6b541dc3e8e29e10b4078ce21612eaa13
Hanif yogatama <hanifyotama@gmail.com>
2020-07-26 01:06:29 +0700

    add splash screen (fixed)

--- app/src/main/java/com/binar/code_challenge_layouting_basic/spalsh_screen.kt
+++ app/src/main/java/com/binar/code_challenge_layouting_basic/spalsh_screen.kt
@@ -1,11 +1,23 @@
 package com.binar.code_challenge_layouting_basic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.lang.Exception
 
 class spalsh_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spalsh_screen)
+        try {
+            this.supportActionBar?.hide()
+        } catch (e: NullPointerException) {}
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },6000)
     }
 }
